3b8c8b44b0a251ff230c562c69d84991a07461d8
kuroneki <kkazyto@gmail.com>
2021-09-30 16:16:43 +0300

    Add PhoneList

---
+++ src/components/PhoneList/index.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class PhoneList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.phoneList = [
+            "8805553535",
+            "0112223344",
+            "0556667788",
+            "0990001122",
+        ];
+    }
+
+    render() {
+
+        const arrayJSX = this.phoneList.map((value) => {
+            const elemJSX = (
+                <li>{value}</li>
+            );
+            return elemJSX;
+        });
+
+        return (
+            <div>
+                <h4>Список контактов</h4>
+                <ul>{arrayJSX}</ul>
+            </div>
+        );
+    }
+}
+
+export default PhoneList;
